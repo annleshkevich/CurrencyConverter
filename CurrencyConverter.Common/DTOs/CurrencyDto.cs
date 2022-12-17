6237ae560863d00140a7eb939181fb30cbc612a2
Your Name <you@example.com>
2022-12-17 21:12:42 +0300

    some changes

--- CurrencyConverter.Common/DTOs/CurrencyDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CurrencyConverter.Common.DTOs
-{
-    public class CurrencyDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Code { get; set; } = string.Empty;
-    }
-}
